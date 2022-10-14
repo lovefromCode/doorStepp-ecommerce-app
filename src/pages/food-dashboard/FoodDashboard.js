61076bc027d0b65da74a633659662787110d46c5
Prashant Maitra <prashantmaitra@smileadmins-MacBook-Pro.local>
2022-10-14 23:27:57 +0530

    login merge in develop

---
+++ src/pages/food-dashboard/FoodDashboard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from '../components/Header';
+
+function FoodDashboard() {
+  return (
+    <>
+      <Header />
+      <div>FoodDashboard</div>
+    </>
+  )
+}
+
+export default FoodDashboard;
