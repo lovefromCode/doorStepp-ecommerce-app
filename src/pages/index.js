48b161a4736f652378250ca2b126e34efd8c02e3
Prashant Maitra <prashantmaitra@smileadmins-MacBook-Pro.local>
2022-10-21 22:42:08 +0530

    re design homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,13 @@
-import styles from '../../styles/Home.module.css';
-import Header from './components/Header';
+import Head from "next/head";
+import Header from '../components/Header';
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
+       <Head>
+        <title>DoorStepp</title>
+      </Head>
+
       <Header />
     </div>
   )
