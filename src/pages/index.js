058e21863ba54b02b15e596e01152cba12c02b44
Prashant Maitra <prashantmaitra@smileadmins-MacBook-Pro.local>
2022-10-22 23:48:34 +0530

    change food app to ecommerce app

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,25 @@
 import Head from "next/head";
 import Header from '../components/Header';
+import ProductFeed from "../components/ProductFeed";
+import axios from "axios";
 
-export default function Home() {
+export default function Home({ products }) {
   return (
     <div>
-       <Head>
+      <Head>
         <title>DoorStepp</title>
       </Head>
 
       <Header />
+      <ProductFeed products={products} />
     </div>
   )
+}
+
+export async function getServerSideProps(context) {
+  const products = await fetch('https://fakestoreapi.com/products')
+    .then(res => res.json())
+  return {
+    props: { products }, // will be passed to the page component as props
+  }
 }
